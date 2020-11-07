199c5069812f76201b7909bed2d14241bbfbb5f1
sree <sreelu.ph@gmail.com>
2020-11-07 15:47:01 -0700

    Completed assignment W20B

---
+++ W20B.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: 35.222.127.166    Database: cli_social_media
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.15-MariaDB-1:10.4.15+maria~stretch
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `exploits`
+--
+
+DROP TABLE IF EXISTS `exploits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exploits` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `exploits_FK` (`user_id`),
+  CONSTRAINT `exploits_FK` FOREIGN KEY (`user_id`) REFERENCES `hackers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exploits`
+--
+
+LOCK TABLES `exploits` WRITE;
+/*!40000 ALTER TABLE `exploits` DISABLE KEYS */;
+INSERT INTO `exploits` VALUES (1,'Helooo everyone from hacker J',1),(2,'Hello V',4),(3,'HELLO J',1),(4,'Hello V from J',1),(5,'greetings from F',3),(6,'greetings from A',2),(7,'greetings from V',4),(8,'helloooooooo V from J',1),(9,'Hellooooo F from J',1),(10,'heli J from bubble',6),(11,'Hi bubble from J',1),(12,'BYE FROM J',1),(13,'HIIIIIIIIIIIIIIIII',1),(14,'helo F ',1),(15,'exploit got hacked',1);
+/*!40000 ALTER TABLE `exploits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hackers`
+--
+
+DROP TABLE IF EXISTS `hackers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hackers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `alias` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `hackers_UN` (`alias`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hackers`
+--
+
+LOCK TABLES `hackers` WRITE;
+/*!40000 ALTER TABLE `hackers` DISABLE KEYS */;
+INSERT INTO `hackers` VALUES (1,'J@123','passwordJ'),(2,'A@123','passwordA'),(3,'F@123','passwordF'),(4,'V@123','passwordV'),(5,'Jain','jain123'),(6,'bubble','bubble123'),(7,'dany','dany123'),(8,'Adam','adam123');
+/*!40000 ALTER TABLE `hackers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'cli_social_media'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-07 15:44:49
--- app.py
+++ app.py
@@ -3,47 +3,60 @@ import mariadb
 
 conn=None
 cursor=None
+
+def insert_data():
+    try:
+        content=input("Write your content: ")
+        cursor.execute("INSERT INTO exploits(content,user_id) VALUES(?,?)", [content,user[0],])
+        conn.commit()
+        if(cursor.rowcount==1):
+            print(" Exploit created successfully")
+            print("-----------------------------")
+    except mariadb.ProgrammingError:
+        print("Operation failed")
+        
+def select_data():
+    try:
+        cursor.execute("SELECT content FROM exploits WHERE user_id=?",[user[0],])
+        users=cursor.fetchall()
+        for user_list in users:
+            print(user_list[0])
+            print("--------------------------------")
+    except mariadb.ProgrammingError:
+        print("Operation failed")
+       
+def get_alldata():
+    try:
+        cursor.execute("SELECT * FROM exploits ex INNER JOIN hackers ha ON ex.user_id=ha.id WHERE user_id!=?",[user[0],])   
+        users=cursor.fetchall()
+        for user_list in users:
+            print("Alias: "+str(user_list[4]))
+                
+            print("Content: "+str(user_list[1]))
+            print("------------------")
+    except mariadb.ProgrammingError:
+        print("Operation failed")
+
+def update_exploits():
+    try:
+        id=input("Enter your exploit's id to update: ")
+        content=input("Enter the content to update: ")
+        cursor.execute("UPDATE exploits SET content=? WHERE id=?", [content,id])
+        conn.commit()    
+        if(cursor.rowcount==1):
+            print("Exploit updated successfully")
+        elif(cursor.rowcount==0):
+            print("Update failed")
+
+    except mariadb.ProgrammingError:
+        print("Operation failed")
+
+
+
+
 try:
     conn=mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
     cursor=conn.cursor()
-    def insert_data():
-        try:
-            content=input("Write your content: ")
-            cursor.execute("INSERT INTO exploits(content,user_id) VALUES(?,?)", [content,user[0],])
-            conn.commit()
-            if(cursor.rowcount==1):
-                print(" Content created successfully")
-                print("-----------------------------")
-        except mariadb.ProgrammingError:
-            print("Operation failed")
-        
-    def select_data():
-        try:
-            cursor.execute("SELECT content FROM exploits WHERE user_id=?",[user[0],])
-            users=cursor.fetchall()
-            for user_list in users:
-                print(user_list[0])
-                print("--------------------------------")
-        except mariadb.ProgrammingError:
-            print("Operation failed")
-       
-    def get_alldata():
-        try:
-            cursor.execute("SELECT * FROM exploits ex INNER JOIN hackers ha ON ex.user_id=ha.id WHERE user_id!=?",[user[0],])   
-            users=cursor.fetchall()
-            for user_list in users:
-                print("Alias: "+str(user_list[4]))
-                
-                print("Content: "+str(user_list[1]))
-                print("------------------")
-                
-
-        except mariadb.ProgrammingError:
-            print("Operation failed")
-       
-
-
-    
     print("1.Login")
     print("2.Sign up")
     choice= input("Enter your choice: ")
@@ -60,20 +73,24 @@ try:
                 print("1.Enter a new exploit")
                 print("2.See all other exploits")
                 print("3.See other's exploits")
-                print("4.Exit")
+                print("4.Update exploits")
+                print("5.Exit")
                 option=input("Enter your option: ")
                 if(option=="1"):
                     insert_data()
-                if(option=="2"):
+                elif(option=="2"):
                     select_data()
-                if(option=="3"):
+                elif(option=="3"):
                     get_alldata()
-                if(option=="4"):
+                elif(option=="4"):
+                    update_exploits()
+                if(option=="5"):
                     break
 
 
         else:
             print("no matching user")
+
     if(choice=="2"):
         alias=input("Enter your alias: ")
         password=input("Enter your password: ")
